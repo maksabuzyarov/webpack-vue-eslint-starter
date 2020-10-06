bdfcff35d4ece24bd68b69d6c376cd287b319aae
MaksAbuzyarov <maksim.abuzyarov@gmail.com>
2020-10-06 22:49:10 +0300

    Moved script directories to top level src

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: './src/scripts/index.js',
+    main: './src/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
